f0e8f3e8fb7e13eb2457dde1142b0a09a91f016e
Luis Carlos Kristen <luiskristen@gmail.com>
2020-04-24 13:45:44 +0200

    ejemplo next

--- src/frontend/routes/App.js
+++ src/frontend/routes/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
@@ -6,6 +7,22 @@ import Register from '../containers/Register';
 import NotFound from '../containers/NotFound';
 import Player from '../containers/Player';
 import Layout from '../components/Layout';
+
+// const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate;
+// renderMethod(
+//   <BrowserRouter>
+//     <Layout>
+//       <Switch>
+//         <Route exact path='/' component={Home} />
+//         <Route exact path='/login' component={Login} />
+//         <Route exact path='/register' component={Register} />
+//         <Route exact path='/player/:id' component={Player} />
+//         <Route component={NotFound} />
+//       </Switch>
+//     </Layout>
+//   </BrowserRouter>,
+//   document.getElementById('root'),
+// );
 
 const App = () => (
   <BrowserRouter>
